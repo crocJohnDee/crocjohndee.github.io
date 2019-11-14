108415b9e73245e919ca3c89f3d35ff37edd7251
crocJohnDee <johnn.barendrecht@hotmail.com>
2019-11-14 12:25:09 +0100

    swapped insta for mail

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,40 +1,38 @@
 import React from "react";
 import "./sass/footer/footer.scss";
 export const Footer = () => {
-    return (
-        <>
-            <footer id="footer">
-                <p>
-                    <span>
-                        <a className="bb"
-                            href="https://www.linkedin.com/in/johnny-barendrecht/"
-                            target="_blank"
-                            rel="noreferrer noopener"
-                        >
-                            <i className="fab fa-linkedin"></i>
-                        </a>
-                    </span>
-                    <span>
-                        <a className="bb"
-                            href="https://github.com/crocJohnDee"
-                            target="_blank"
-                            rel="noreferrer noopener"
-                        >
-                            <i className="fab fa-github"></i>
-                        </a>
-                    </span>
-                    <span>
-                        <a className="bb"
-                            href="https://www.instagram.com/johnnybarendrecht"
-                            target="_blank"
-                            rel="noreferrer noopener"
-                        >
-                            <i className="fab fa-instagram"></i>
-                        </a>
-                    </span>
-                </p>
-                <p id="footer-text">&copy; Johnny Barendrecht - 2019 | web-developer</p>
-            </footer>
-        </>
-    );
+  return (
+    <>
+      <footer id="footer">
+        <p>
+          <span>
+            <a
+              className="bb"
+              href="https://www.linkedin.com/in/johnny-barendrecht/"
+              target="_blank"
+              rel="noreferrer noopener"
+            >
+              <i className="fab fa-linkedin"></i>
+            </a>
+          </span>
+          <span>
+            <a
+              className="bb"
+              href="https://github.com/crocJohnDee"
+              target="_blank"
+              rel="noreferrer noopener"
+            >
+              <i className="fab fa-github"></i>
+            </a>
+          </span>
+          <span>
+            <a className="bb" href="mailto:johnny.barendrecht@gmail.com">
+              <i className="far fa-envelope"></i>
+            </a>
+          </span>
+        </p>
+        <p id="footer-text">&copy; Johnny Barendrecht - 2019 | web-developer</p>
+      </footer>
+    </>
+  );
 };
