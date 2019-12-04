9726630dfbe71d9086c92b76802b90377b84e87b
crocJohnDee <johnn.barendrecht@hotmail.com>
2019-12-04 08:25:36 +0100

    Updates

---
+++ precache-manifest.19c28a5548457ac5694c962432e7544d.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23a1df705991b07fae7db167aba08f9f",
+    "url": "/johnnybarendrecht/index.html"
+  },
+  {
+    "revision": "1ba7c127c0b8ea23b6ce",
+    "url": "/johnnybarendrecht/static/css/2.68e2c45d.chunk.css"
+  },
+  {
+    "revision": "3f384e0a2dc9f70372cc",
+    "url": "/johnnybarendrecht/static/css/main.0cabf796.chunk.css"
+  },
+  {
+    "revision": "1ba7c127c0b8ea23b6ce",
+    "url": "/johnnybarendrecht/static/js/2.b91948e9.chunk.js"
+  },
+  {
+    "revision": "3f384e0a2dc9f70372cc",
+    "url": "/johnnybarendrecht/static/js/main.c12762be.chunk.js"
+  },
+  {
+    "revision": "f59fbb5622de9500fad6",
+    "url": "/johnnybarendrecht/static/js/runtime~main.367332cb.js"
+  },
+  {
+    "revision": "7bc86dd4698e3e77ad01eb87b5b14eda",
+    "url": "/johnnybarendrecht/static/media/Johnny Barendrecht.7bc86dd4.pdf"
+  },
+  {
+    "revision": "9fd2ddbe354540f946ee9f717685a65b",
+    "url": "/johnnybarendrecht/static/media/Johnny Barendrecht.9fd2ddbe.png"
+  },
+  {
+    "revision": "1d60d41c663a15bdea56ee913355bb60",
+    "url": "/johnnybarendrecht/static/media/armin.1d60d41c.jpg"
+  },
+  {
+    "revision": "3abdc6958ca62646e0ff183dca2c6257",
+    "url": "/johnnybarendrecht/static/media/available.3abdc695.png"
+  },
+  {
+    "revision": "a697f84323a9e9cbf748be6c86fb7244",
+    "url": "/johnnybarendrecht/static/media/background.a697f843.jpg"
+  },
+  {
+    "revision": "bbdf696be82b730aa7617e20d310e01f",
+    "url": "/johnnybarendrecht/static/media/bill-gates.bbdf696b.jpg"
+  },
+  {
+    "revision": "3d771f15e7f21c77d76e4d7cb1a9dd8e",
+    "url": "/johnnybarendrecht/static/media/brian.3d771f15.jpg"
+  },
+  {
+    "revision": "8175858cc86edb4d3f5524635e1ada32",
+    "url": "/johnnybarendrecht/static/media/elon.8175858c.jpg"
+  },
+  {
+    "revision": "0425d661f34ffa46604c9dfa344c03bb",
+    "url": "/johnnybarendrecht/static/media/fa-brands-400.0425d661.woff2"
+  },
+  {
+    "revision": "189c0326f75931886d5b60cde8b5628f",
+    "url": "/johnnybarendrecht/static/media/fa-brands-400.189c0326.ttf"
+  },
+  {
+    "revision": "3a19eb0134e93ea0caa5826f5fb7b642",
+    "url": "/johnnybarendrecht/static/media/fa-brands-400.3a19eb01.svg"
+  },
+  {
+    "revision": "451b51d8700fb035bcaaa9e8710b384a",
+    "url": "/johnnybarendrecht/static/media/fa-brands-400.451b51d8.eot"
+  },
+  {
+    "revision": "85b063debeb5d5f079776ac1a9c924cb",
+    "url": "/johnnybarendrecht/static/media/fa-brands-400.85b063de.woff"
+  },
+  {
+    "revision": "14f22fbed8146dbf3b497bb32d8491b0",
+    "url": "/johnnybarendrecht/static/media/fa-regular-400.14f22fbe.woff"
+  },
+  {
+    "revision": "772a0f14c850c8b0dfe283a95857583e",
+    "url": "/johnnybarendrecht/static/media/fa-regular-400.772a0f14.woff2"
+  },
+  {
+    "revision": "8cd2768111e3091948920a3ad11f96c6",
+    "url": "/johnnybarendrecht/static/media/fa-regular-400.8cd27681.svg"
+  },
+  {
+    "revision": "9230982faf6ed4d1dab07babfb813777",
+    "url": "/johnnybarendrecht/static/media/fa-regular-400.9230982f.ttf"
+  },
+  {
+    "revision": "b6579220162705be426ad4db0dc43f6b",
+    "url": "/johnnybarendrecht/static/media/fa-regular-400.b6579220.eot"
+  },
+  {
+    "revision": "1898ebd2e3162e7ca8627073d865b893",
+    "url": "/johnnybarendrecht/static/media/fa-solid-900.1898ebd2.eot"
+  },
+  {
+    "revision": "1d825f28ae4e412886fb0123b94a8ac2",
+    "url": "/johnnybarendrecht/static/media/fa-solid-900.1d825f28.svg"
+  },
+  {
+    "revision": "4cc04a31c42f2f9d951547bbce75960b",
+    "url": "/johnnybarendrecht/static/media/fa-solid-900.4cc04a31.woff2"
+  },
+  {
+    "revision": "b1161bce5381a5fa7aacc073e1a01dd3",
+    "url": "/johnnybarendrecht/static/media/fa-solid-900.b1161bce.ttf"
+  },
+  {
+    "revision": "db525089f5101b59a66184d109e6f917",
+    "url": "/johnnybarendrecht/static/media/fa-solid-900.db525089.woff"
+  },
+  {
+    "revision": "018016828f2afc89a750706ed999fa0a",
+    "url": "/johnnybarendrecht/static/media/me.01801682.png"
+  },
+  {
+    "revision": "952683c17f00d38f0f0eef2e87bfdb72",
+    "url": "/johnnybarendrecht/static/media/neil.952683c1.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.55057d8c97121325e74cf1eaa9813d5d.js"
+  "/johnnybarendrecht/precache-manifest.19c28a5548457ac5694c962432e7544d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/johnnybarendrecht/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
