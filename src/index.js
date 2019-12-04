f7bca453e649f1822762c938121a0304589b85a7
crocJohnDee <johnn.barendrecht@hotmail.com>
2019-12-04 08:22:59 +0100

    changed homepage

--- src/index.js
+++ src/index.js
@@ -2,16 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import { HashRouter } from "react-router-dom";
-setTimeout(() => {
-  const nav = document.querySelector("nav");
 
-  console.dir(nav.baseURI);
-  nav.classList.remove("nav-colored");
-
-  window.onscroll = function() {
-    nav.classList.add("nav-colored");
-  };
-});
 ReactDOM.render(
   <HashRouter>
     <App />
